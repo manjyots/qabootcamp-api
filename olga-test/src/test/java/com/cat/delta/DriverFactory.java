7314779772a7c51e7f990794b3a3149fb2d044d2
Nikhil Jain <jain0nikhil@gmail.com>
2017-11-27 11:53:09 +0530

    Adding Appium setup.

--- olga-test/src/test/java/com/cat/delta/DriverFactory.java
+++ olga-test/src/test/java/com/cat/delta/DriverFactory.java
@@ -1,11 +1,20 @@
 package com.cat.delta;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.IOSMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import utils.ReadProjectProperty;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.System.getProperty;
 
@@ -13,28 +22,63 @@ public class DriverFactory {
     private String driverType;
 
     private boolean enableJavascript = true;
+    ReadProjectProperty readProjectProperty = null;
 
-    public WebDriver loadDriver(String browser) throws InterruptedException, IOException {
-
-        return loadDriver(enableJavascript, browser);
+    public WebDriver loadDriver(ReadProjectProperty readProjectProperty) throws InterruptedException, IOException {
+        this.readProjectProperty = readProjectProperty;
+        if (readProjectProperty.getSpecificProjectProperty("mode").equals("web")) {
+            return loadDriverForWeb(readProjectProperty);
+        } else {
+            return loadDriverForMobile(readProjectProperty);
+        }
     }
 
-    public WebDriver loadDriver(boolean enableJavascript, String browser) throws InterruptedException, IOException {
-            driverType = getProperty("web.driver", "Firefox");
+
+    public WebDriver loadDriverForWeb(ReadProjectProperty readProjectProperty) throws InterruptedException, IOException {
+        if (readProjectProperty.getSpecificProjectProperty("driver").equals("firefox")) {
             return createFirefoxDriver(enableJavascript);
+        } else {
+            return createChromeDriver(enableJavascript);
+        }
     }
 
+
+    public WebDriver loadDriverForMobile(ReadProjectProperty readProjectProperty) throws InterruptedException, IOException {
+        DesiredCapabilities dc = new DesiredCapabilities();
+        WebDriver driver = null;
+        if (readProjectProperty.getSpecificProjectProperty("driver").equals("android")) {
+            return null;
+        } else if (readProjectProperty.getSpecificProjectProperty("driver").equals("ios")) {
+            dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, readProjectProperty.getSpecificProjectProperty("osVersion"));
+            dc.setCapability(MobileCapabilityType.DEVICE_NAME, readProjectProperty.getSpecificProjectProperty("deviceName"));
+            dc.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, readProjectProperty.getSpecificProjectProperty("timeout"));
+            dc.setCapability("locationServicesAuthorized", true);
+            dc.setCapability("autoAcceptAlerts", true);
+            dc.setCapability(MobileCapabilityType.PLATFORM_NAME, readProjectProperty.getSpecificProjectProperty("driver"));
+            dc.setCapability(IOSMobileCapabilityType.AUTO_ACCEPT_ALERTS, Boolean.TRUE);
+            dc.setCapability(MobileCapabilityType.APP, readProjectProperty.getSpecificProjectProperty("appPath"));
+            driver = new IOSDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), dc);
+            ((AppiumDriver) driver).manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+            return driver;
+        }else{
+            return null;
+        }
+    }
 
 
     private WebDriver createFirefoxDriver(boolean enableJavascript) throws IOException {
-        ReadProjectProperty readProjectProperty = ReadProjectProperty.getInstance();
-        String BASE_URL = readProjectProperty.getSpecificProjectProperty("DELTA_URL");
+
         FirefoxProfile profile = new FirefoxProfile();
         profile.setAcceptUntrustedCertificates(true);
         profile.setPreference("signed.applets.codebase_principal_support", true);
         profile.setPreference("javascript.enabled", enableJavascript);
         profile.setEnableNativeEvents(true);
         return new FirefoxDriver(profile);
+    }
+
+    private WebDriver createChromeDriver(boolean enableJavascript) throws IOException {
+
+        return null;
     }
 
 
--- olga-test/src/test/java/com/cat/delta/ManageTest.java
+++ olga-test/src/test/java/com/cat/delta/ManageTest.java
@@ -11,21 +11,19 @@ public class ManageTest {
     protected static final String DEFAULT_BROWSER = "firefox";
 
     protected static TestWebDriver testWebDriver;
-    protected static TestWebDriver tempTestWebDriver;
-    protected static TestWebDriver testWebAdminDriver;
     public static String BASE_URL;
-
+    ReadProjectProperty readProjectProperty = null;
 
 
 
     public ManageTest() throws IOException, InterruptedException {
-        ReadProjectProperty readProjectProperty = ReadProjectProperty.getInstance();
+         readProjectProperty = ReadProjectProperty.getInstance();
         BASE_URL = readProjectProperty.getSpecificProjectProperty("DELTA_URL");
 
     }
 
     protected void loadDriver(String browser) throws InterruptedException, IOException {
-        testWebDriver = new TestWebDriver(driverFactory.loadDriver(browser));
+        testWebDriver = new TestWebDriver(driverFactory.loadDriver(readProjectProperty));
     }
 
 
